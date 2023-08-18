8fa092c422caba38e8549a9bb047be71f4a9ca2a
μEDH <124815001+uedh-net@users.noreply.github.com>
2023-08-18 14:32:12 -0400

    Update card_viewer.js

--- assets/js/card_viewer.js
+++ assets/js/card_viewer.js
@@ -1,11 +1,32 @@
-// Silence is golden.
-// e.g., https://api.scryfall.com/cards/clb/48?format=image&version=normal
+function card_href_to_image_req (href) {
+  // e.g.,
+  // https://scryfall.com/card/clb/48/veteran-soldier ->
+  // https://api.scryfall.com/cards/clb/48?format=image&version=normal
+  // remove 'https://'
+  if (href.slice(0, 8) !== 'https://') return
+  href = href.slice(8, foo.length)
+  // add 'api.'
+  href = 'api.' + href
+  // 'card' -> 'cards'
+  var parts = href.split('/')
+  if (parts[1] !== 'card') return
+  parts[1] = 'cards'
+  // remove name
+  parts.pop()
+  href = parts.join('/')
+  // add params
+  href = href + '?format=image&version=normal'
+  return href
+}
+
 (function () {
   const content = document.getElementById('content')
   var cards = content.getElementsByClassName('card')
   for (var i = 0; i < cards.length; i++) {
     var card = cards[i]
-    var uri = card.href
-    console.log(uri)
+    var href = card.href
+    var image_req = card_href_to_image_req(href)
+    if (image_req === null) continue
+    console.log(image_req)
   }
 })()
