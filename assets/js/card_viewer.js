7e1c56fc8368832f787d1866440789eecfaa3dc3
μEDH <124815001+uedh-net@users.noreply.github.com>
2023-08-18 14:14:23 -0400

    Update card_viewer.js

--- assets/js/card_viewer.js
+++ assets/js/card_viewer.js
@@ -1,8 +1,10 @@
 // Silence is golden.
 // e.g., https://api.scryfall.com/cards/clb/48?format=image&version=normal
 (function () {
-  var cards = document.getElementsByClassName("card")
+  console.log('iife')
+  var cards = document.getElementsByClassName('card')
   for (var i = 0; i < cards.length; i++) {
+    console.log(i)
     var card = cards[i]
     var uri = anchor.href
     console.log(uri)
