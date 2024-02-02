8a9e772bc8ea9b46daea0af01f9bc8a5ed9f950f
μEDH <124815001+uedh-net@users.noreply.github.com>
2024-02-01 21:58:46 -0500

    Update card_viewer.js

--- assets/js/card_viewer.js
+++ assets/js/card_viewer.js
@@ -21,27 +21,47 @@ function card_to_image (href) {
   return href
 }
 
-function show_on_hover (viewer, card, image) {
-  card.addEventListener('mouseover', () => {
-    viewer.innerHTML = '<img src="' + image + '" />'
-    FloatingUIDOM.computePosition(card, viewer).then(({x, y}) => {
-      Object.assign(viewer.style, {
+function attach_tooltip_event_handlers (element, tooltip, image) {
+  function update_tooltip() {
+    tooltip.innerHTML = '<img src="' + image + '" />'
+    FloatingUIDOM.computePosition(element, tooltip, {
+      // placement: 'top',
+      middleware: [FloatingUIDOM.flip(), FloatingUIDOM.shift()],
+    }).then(({x, y}) => {
+      Object.assign(tooltip.style, {
         left: `${x}px`,
         top: `${y}px`,
       })
     })
+  }
+
+  function show_tooltip() {
+    tooltip.style.display = 'block'
+    update_tooltip()
+  }
+   
+  function hide_tooltip() {
+    tooltip.style.display = ''
+  }
+
+  [
+    ['mouseenter', show_tooltip],
+    ['mouseleave', hide_tooltip],
+    ['focus', show_tooltip],
+    ['blur', hide_tooltip],
+  ].forEach(([event, listener]) => {
+    element.addEventListener(event, listener)
   })
 }
 
 (function () {
   const viewer = document.getElementById('card_viewer')
-  const content = document.getElementById('content')
   var cards = content.getElementsByClassName('card')
   for (var i = 0; i < cards.length; i++) {
     var card = cards[i]
     var href = card.href
     var image = card_to_image(href)
     if (image === null) continue
-    show_on_hover(viewer, card, image)
+    attach_tooltip_event_handlers(card, viewer, image)
   }
 })()
