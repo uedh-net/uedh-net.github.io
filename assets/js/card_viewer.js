e0e4d8dc4ff3cd1521adc603478888cf92e1ddc7
μEDH <124815001+uedh-net@users.noreply.github.com>
2024-02-04 16:08:07 -0500

    Update card_viewer.js

--- assets/js/card_viewer.js
+++ assets/js/card_viewer.js
@@ -1,7 +1,7 @@
 function card_to_image (href) {
   // e.g.,
-  // https://scryfall.com/card/clb/48/veteran-soldier ->
-  // https://api.scryfall.com/cards/clb/48?format=image&version=normal
+  //   https://scryfall.com/card/clb/48/veteran-soldier ->
+  //   https://api.scryfall.com/cards/clb/48?format=image&version=normal
   // remove 'https://'
   if (href.slice(0, 8) !== 'https://') return
   href = href.slice(8, href.length)
@@ -23,7 +23,6 @@ function card_to_image (href) {
 
 function attach_tooltip_event_handlers (element, tooltip, image) {
   function update_tooltip() {
-    tooltip.innerHTML = '<img src="' + image + '" />'
     FloatingUIDOM.computePosition(element, tooltip, {
       placement: 'right',
       middleware: [FloatingUIDOM.flip(), FloatingUIDOM.shift()],
@@ -53,13 +52,14 @@ function attach_tooltip_event_handlers (element, tooltip, image) {
 }
 
 (function () {
-  const viewer = document.getElementById('card_viewer')
   var cards = content.getElementsByClassName('card')
   for (var i = 0; i < cards.length; i++) {
     var card = cards[i]
-    var href = card.href
-    var image = card_to_image(href)
+    var image = card_to_image(card.href)
     if (image === null) continue
-    attach_tooltip_event_handlers(card, viewer, image)
+    var tooltip_id = 'tooltip_' + i
+    card.insertAdjacentHTML('afterend', '<div id="' + tooltip_id + '" class="tooltip"><img src="' + image + '" /></div>')
+    var tooltip = document.getElementById(tooltip_id)
+    attach_tooltip_event_handlers(card, tooltip, image)
   }
 })()
